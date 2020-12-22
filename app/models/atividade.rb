34ee93796ffdc46bf8e9fada4c6554aa4368b757
diogoloiola <diogoloiola666@gmail.com>
2020-12-22 19:50:45 -0300

    casos de teste feitos e configuracao para realizacao dos mesmoss

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -1,3 +1,10 @@
 class Atividade < ApplicationRecord
+    validates_presence_of :nomeAtividade, message: 'nao pode ser deixado em branco'
+    validates_presence_of :conteudoAtividade, message: 'nao pode ser deixado em branco'
+    validates_presence_of :pessoaResponsavel, message: 'nao pode ser deixado em branco'
+
+    validates_length_of :nomeAtividade, minimum: 5, message: 'deve ter pelo menos 3 caracteres'
+    validates_length_of :conteudoAtividade, minimum: 6, message: 'deve ter pelo menos 3 caracteres'
+    validates_length_of :pessoaResponsavel, minimum: 3, message: 'deve ter pelo menos 3 caracteres'
     belongs_to :usuario
 end
--- test/models/atividade_test.rb
+++ test/models/atividade_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class AtividadeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Verificando se a instancia o obj normal" do
+     assert Atividade.new.present?
+  end
+  test "Objetio sem o conteudo Atividade" do
+   flag = Atividade.new(nomeAtividade: "teste", pessoaResponsavel: "maria").invalid?
+   assert(flag == true, 'nao pode ser deixado em branco')
+  end  
+  test "Objetio sem a pessoa resonsavel" do
+    flag = Atividade.new(conteudoAtividade: "isso é uma atividade que de ser entregue ate sexta", pessoaResponsavel: "maria").invalid?
+    assert(flag == true, 'nao pode ser deixado em branco')
+   end
+
+  test "Objetio sem a pessoa resonsavel e sem o nome da atividade" do
+    flag = Atividade.new(conteudoAtividade: "isso é uma atividade que de ser entregue ate sexta").invalid?
+    assert(flag == true, 'nao pode ser deixado em branco')
+   end
+
+   test "Objetio so com pessoa responsavel" do
+    flag = Atividade.new(pessoaResponsavel: "maria").invalid?
+    assert(flag == true, 'nao pode ser deixado em branco')
+   end
+
+   test "Objetio so com o nome da atividade" do
+    flag = Atividade.new(nomeAtividade: "Fazer planilhas").invalid?
+    assert(flag == true, 'nao pode ser deixado em branco')
+   end
+
+   test "Objetio com o nome da ativiade menor que 5" do
+    flag = Atividade.new(nomeAtividade: "abcd").invalid?
+    assert(flag == true)
+   end
+
+   test "Objetio com o conteudo atividade menor que 6" do
+      flag = Atividade.new(conteudoAtividade: "agora").invalid?
+      assert(flag == true)
+   end
+
+   test "Objetio com o pessoa responsavel menor que 3" do
+    flag = Atividade.new(pessoaResponsavel: "du").invalid?
+    assert(flag == true)
+   end
+
 end
